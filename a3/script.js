d48d144657f1869def6b3da9e430911b938b9213
CristinaaVega <cristina.vega@bellevuecollege.edu>
2025-02-13 13:54:35 -0800

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -6,24 +6,28 @@ function generateRhombus() {
 
     let rhombusHTML = "";
 
-    // Generate top half of rhombus
+    // Generate top half
     for (let i = 0; i < height; i++) {
-        let lineHTML = "&nbsp;".repeat(height - i); // Spaces for alignment
+        let spaces = " ".repeat(height - i); // Spaces for alignment
+        let lineHTML = spaces;
+
         for (let j = 0; j <= i * 2; j++) {
             let color = j % 2 === 0 ? colorEven : colorOdd;
             lineHTML += `<span style="color:${color};">${symbol}</span>`;
         }
-        rhombusHTML += `<div>${lineHTML}</div>`;
+        rhombusHTML += `<div style="white-space: pre;">${lineHTML}</div>`;
     }
 
-    // Generate bottom half of rhombus
+    // Generate bottom half
     for (let i = height - 2; i >= 0; i--) {
-        let lineHTML = "&nbsp;".repeat(height - i); // Spaces for alignment
+        let spaces = " ".repeat(height - i); // Spaces for alignment
+        let lineHTML = spaces;
+
         for (let j = 0; j <= i * 2; j++) {
             let color = j % 2 === 0 ? colorEven : colorOdd;
             lineHTML += `<span style="color:${color};">${symbol}</span>`;
         }
-        rhombusHTML += `<div>${lineHTML}</div>`;
+        rhombusHTML += `<div style="white-space: pre;">${lineHTML}</div>`;
     }
 
     // Set the rhombus in the container
