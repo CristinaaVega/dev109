9783144589082d555c289bac6949831fe0c0c010
CristinaaVega <cristina.vega@bellevuecollege.edu>
2025-02-13 13:51:51 -0800

    Update script.js

--- a3/script.js
+++ a3/script.js
@@ -4,55 +4,30 @@ function generateRhombus() {
     let colorOdd = document.getElementById("colorOdd").value;
     let symbol = document.getElementById("symbol").value;
 
-    let rhombusLeft = "";
-    let rhombusRight = "";
+    let rhombusHTML = "";
 
+    // Generate top half of rhombus
     for (let i = 0; i < height; i++) {
-        let leftSpaces = "&nbsp;".repeat(height - i); // Spaces for left side alignment
-        let rightSpaces = "&nbsp;".repeat(i * 2); // Spaces to bring right side closer
-        
-        rhombusLeft += "<p>" + leftSpaces;
-        rhombusRight += "<p>";
-
-        for (let j = 0; j <= i; j++) {
-            let color = j % 2 === 0 ? colorOdd : colorEven;
-            rhombusLeft += `<span style='color:${color};'>${symbol}</span> `;
+        let lineHTML = "&nbsp;".repeat(height - i); // Spaces for alignment
+        for (let j = 0; j <= i * 2; j++) {
+            let color = j % 2 === 0 ? colorEven : colorOdd;
+            lineHTML += `<span style="color:${color};">${symbol}</span>`;
         }
-        for (let j = 0; j <= i; j++) {
-            let color = j % 2 === 0 ? colorOdd : colorEven;
-            rhombusRight += `<span style='color:${color};'>${symbol}</span> `;
-        }
-
-        rhombusLeft += "</p>";
-        rhombusRight += "</p>";
+        rhombusHTML += `<div>${lineHTML}</div>`;
     }
 
-    let rhombusBottomLeft = "";
-    let rhombusBottomRight = "";
-
-    for (let i = height; i > 0; i--) {
-        let leftSpaces = "&nbsp;".repeat(height - i); // Spaces for left side alignment
-        let rightSpaces = "&nbsp;".repeat(i * 2); // Spaces to bring right side closer
-        
-        rhombusBottomLeft += "<p>" + leftSpaces;
-        rhombusBottomRight += "<p>";
-
-        for (let j = 0; j < i; j++) {
-            let color = j % 2 === 0 ? colorOdd : colorEven;
-            rhombusBottomLeft += `<span style='color:${color};'>${symbol}</span> `;
+    // Generate bottom half of rhombus
+    for (let i = height - 2; i >= 0; i--) {
+        let lineHTML = "&nbsp;".repeat(height - i); // Spaces for alignment
+        for (let j = 0; j <= i * 2; j++) {
+            let color = j % 2 === 0 ? colorEven : colorOdd;
+            lineHTML += `<span style="color:${color};">${symbol}</span>`;
         }
-        for (let j = 0; j < i; j++) {
-            let color = j % 2 === 0 ? colorOdd : colorEven;
-            rhombusBottomRight += `<span style='color:${color};'>${symbol}</span> `;
-        }
-
-        rhombusBottomLeft += "</p>";
-        rhombusBottomRight += "</p>";
+        rhombusHTML += `<div>${lineHTML}</div>`;
     }
 
-    document.getElementById("upLeft").innerHTML = rhombusLeft;
-    document.getElementById("upRight").innerHTML = rhombusRight;
-    document.getElementById("downLeft").innerHTML = rhombusBottomLeft;
-    document.getElementById("downRight").innerHTML = rhombusBottomRight;
+    // Set the rhombus in the container
+    document.getElementById("rhombusContainer").innerHTML = rhombusHTML;
 }
+
 
