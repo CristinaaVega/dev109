c94c1095222b3b63f3e962a01a06505774b1172c
CristinaaVega <cristina.vega@bellevuecollege.edu>
2025-02-22 02:02:43 -0800

    Update java.js

--- m07/js/java.js
+++ m07/js/java.js
@@ -8,24 +8,22 @@ function Hotel(name, rooms, booked) {
   };
 }
 
-
-// Create two hotel objects
+// Create three hotel objects (added Sunset Hotel)
 var quayHotel = new Hotel('Quay', 40, 25);
 var parkHotel = new Hotel('Park', 120, 77);
-
+var sunsetHotel = new Hotel('Sunset', 86, 10); // New hotel
 
 // Update the HTML for the page
-var details1 = quayHotel.name + ' rooms: ';
-    details1 += quayHotel.checkAvailability();
-var elHotel1 = document.getElementById('hotel1');
-elHotel1.textContent = details1;
+var details1 = quayHotel.name + ' rooms: ' + quayHotel.checkAvailability();
+document.getElementById('hotel1').textContent = details1;
 
-var details2 = parkHotel.name + ' rooms: ';
-    details2 += parkHotel.checkAvailability();
-var elHotel2 = document.getElementById('hotel2');
-elHotel2.textContent = details2;
+var details2 = parkHotel.name + ' rooms: ' + parkHotel.checkAvailability();
+document.getElementById('hotel2').textContent = details2;
+
+var details3 = sunsetHotel.name + ' rooms: ' + sunsetHotel.checkAvailability();
+document.getElementById('hotel3').textContent = details3; // Updated to display Sunset hotel
 
 /* 
 NOTE: textContent does not work in IE8 or earlier
-You can use innerHTML on lines 21 and 26, but note the security issues on p228-231
+You can use innerHTML on lines 21, 24, and 27, but note the security issues on p228-231
 */
